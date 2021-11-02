23d6522e5ad7943912c1bb49a62e735c5561bc28
Arkady <arkadyzyganov@gmail.com>
2021-11-02 10:20:22 +0200

    update

--- task2.rb
+++ task2.rb
@@ -39,26 +39,36 @@ class CarManager
 		return selected
 	end
 
+	def toString
+		res = ''
+		for car in $cars do
+			res += car.to_s + "\n"
+		end
+		return res
+	end
+
 end
 
 manager = CarManager.new
-manager.add Car.new(1, 'Honda', 'XX', 2011, 'r', 300)
-manager.add Car.new(2, 'Honda', 'XX', 2010, 'r', 150)
-manager.add Car.new(3, 'Mazda', 'XX', 2010, 'r', 300)
-manager.add Car.new(4, 'Honda', 'YY', 2011, 'r', 400)
+manager.add Car.new(1, 'Honda', 'XX', 2011, 'r', 300, '001')
+manager.add Car.new(2, 'Honda', 'XX', 2010, 'r', 150, '002')
+manager.add Car.new(3, 'Mazda', 'XX', 2010, 'r', 300, '003')
+manager.add Car.new(4, 'Honda', 'YY', 2011, 'r', 400, '004')
 
 # # manager.add car
 arr = manager.slect_brend('Honda')
 for car1 in arr do
 	puts car1
 end
-p ' '
+puts "\n"
 arr = manager.select_model_older_n('XX', 5)
 for car1 in arr do
 	puts car1
 end
-p ' '
+puts "\n"
 arr = manager.slect_by_year_and_price(2010, 200)
 for car1 in arr do
 	puts car1
 end
+
+puts manager.toString
